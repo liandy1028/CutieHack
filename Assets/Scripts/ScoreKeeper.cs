e8b50506cd61568efb0086062c79ee5cb61b59b6
MSI\arthu <arthurgan29@gmail.com>
2021-11-06 16:16:22 -0700

    asdf

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ScoreKeeper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
