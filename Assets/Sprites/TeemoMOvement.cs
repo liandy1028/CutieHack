aeabe6c173b7ce27feda3df623d18c784242972e
MSI\arthu <arthurgan29@gmail.com>
2021-11-06 11:07:20 -0700

    test

---
+++ Assets/Sprites/TeemoMOvement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeemoMOvement : MonoBehaviour
+{
+    private float moveSpeed;
+    private bool moveRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 2f;
+        moveRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > 7f)
+        {
+            moveRight = false;
+        }
+        else if ( transform.position.x < -7f)
+        {
+            moveRight = true;
+        }
+
+        if (moveRight)
+        {
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime,
+                transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime,
+                transform.position.y);
+        }
+    }
+}
