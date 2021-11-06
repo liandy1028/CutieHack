e9d392c83ef961545466dfdd41b2a235672c33f2
Andy Li <liandy1028@gmail.com>
2021-11-06 16:00:44 -0700

    removed prints

--- Assets/Scripts/mushroombullet.cs
+++ Assets/Scripts/mushroombullet.cs
@@ -19,9 +19,7 @@ public class mushroombullet : Bullet
     void Update() {
         if (!straight) {
             Vector3 direction = player.transform.position - transform.position;
-            print(direction);
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x) - 90f);
-            print(moveDirection);
         }
     }
 
