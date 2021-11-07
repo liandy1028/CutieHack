5964d2c4b783f336075e0b9cbef4906aa0e3c81f
jesche487 <jesche487@gmail.com>
2021-11-06 17:33:35 -0800

    haha get zoinked

--- Assets/Scripts/PlayerEntity.cs
+++ Assets/Scripts/PlayerEntity.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerEntity : Entity
 {
     public LayerMask hazardMask;
     public ParticleSystem deathParticles;
+
+    public Text DeathMessage;
     void OnCollisionEnter2D(Collision2D coll)
     {
         if(1 << coll.gameObject.layer == (1 << coll.gameObject.layer & hazardMask.value))
@@ -13,12 +17,20 @@ public class PlayerEntity : Entity
             EntityDie();
         }
     }
-
     protected override void EntityDie()
     {
         base.EntityDie();
         GetComponent<SpriteRenderer>().enabled = false;
         deathParticles.Play();
         gameObject.SetActive(false);
+
+        DeathMessage.enabled = true;
+        Invoke("gamereset",2f);
+    }
+
+    void gamereset()
+    {
+        DeathMessage.enabled = false;
+        SceneManager.LoadScene("SampleScene");
     }
 }
